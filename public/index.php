6eba82f15f9ccca04cbd43852b94015b6bf9e825
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-11-02 16:11:21 +0100

    add view

--- public/index.php
+++ public/index.php
@@ -19,6 +19,7 @@ $uri = strtok($_SERVER['REQUEST_URI'], '?');
 switch ($uri) {
     case '/':
         echo "Bienvenue sur l'app OAuth !";
+        require_once __DIR__ . '/../src/views/login.html';
         break;
     case '/login':
         require_once __DIR__ . '/../src/routes/login.php';
